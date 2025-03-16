b49bfc69e2d2a0947d62cd86115c4cbfbe4660c8
HighGit <minhhai1312004@gmail.com>
2025-03-16 16:17:03 +1100

    SIT323-2.1P

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+const addTwoNumber = (n1, n2) => {
+    return n1 + n2;
+};
+
+app.get("/addTwoNumber", (req, res) => {
+    const n1 = parseInt(req.query.n1);
+    const n2 = parseInt(req.query.n2);
+
+    // Validate input
+    if (isNaN(n1) || isNaN(n2)) {
+        return res.status(400).json({ statuscode: 400, message: "Invalid input. Please enter valid numbers." });
+    }
+
+    const result = addTwoNumber(n1, n2);
+    res.json({ statuscode: 200, data: result });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = 3040;
+app.listen(port, () => {
+    console.log("hello i'm listening to port " + port);
+});
